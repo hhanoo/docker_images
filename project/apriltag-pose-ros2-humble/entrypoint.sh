f730e2d8fbee636646bd950f49f8735ed00c588c
hhanoo <woo980711@gmail.com>
2026-04-06 13:24:45 +0900

    [refactor] Clean up apriltag-pose-ros2-humble setup
    - Remove unnecessary developer HOME directory setup
    - Remove runtime timezone setup from entrypoint
    - Add existence check for ROS 2 setup.bash sourcing

--- project/apriltag-pose-ros2-humble/entrypoint.sh
+++ project/apriltag-pose-ros2-humble/entrypoint.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 set -e
 
-# Set timezone to Seoul
-export TZ=Asia/Seoul
-ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
+# Source ROS2 environment
+if [ -f /opt/ros/humble/setup.bash ]; then
+    source /opt/ros/humble/setup.bash
+fi
 
-# Source ROS2
-source /opt/ros/humble/setup.bash
-
-# Source workspace (if built)
+# Source workspace if built
 if [ -f /ros2_ws/install/setup.bash ]; then
     source /ros2_ws/install/setup.bash
 fi
