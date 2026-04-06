9f89cfd41ac24c1c702bcffb59d61cca0b2ce240
hhanoo <woo980711@gmail.com>
2026-04-06 12:51:42 +0900

    [refactor] Simplify CUDA path and improve entrypoint
    - Use ENV instead of bashrc for CUDA path variables
    - Remove unnecessary developer HOME directory setup
    - Add existence check for ROS 2 setup.bash in entrypoint

--- project/poc-daekeum-humble/entrypoint.sh
+++ project/poc-daekeum-humble/entrypoint.sh
@@ -2,7 +2,9 @@
 set -e
 
 # Source ROS2 environment
-source /opt/ros/humble/setup.bash
+if [ -f /opt/ros/humble/setup.bash ]; then
+    source /opt/ros/humble/setup.bash
+fi
 
 # Source workspace if built
 if [ -f /ros2_ws/install/setup.bash ]; then
